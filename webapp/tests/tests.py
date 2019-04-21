bd75c5d096502df8b68ea0f541adbe41fd63d792
bharathgane <bharath41597@gmail.com>
2019-04-21 18:04:54 +0530

    basic test

---
+++ webapp/tests/tests.py
@@ -0,0 +1,10 @@
+import requests
+import unittest 
+  
+class endpoints(unittest.TestCase): 
+  
+    def test(self):
+    	self.assertEqual(requests.get("http://0.0.0.0:8000/requirements").status_code,200)
+  
+if __name__ == '__main__': 
+    unittest.main()

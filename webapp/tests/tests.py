c797832aae3fc8be0c8037dc9f391d2b8f846c3a
bharathgane <bharath41597@gmail.com>
2019-04-21 23:15:01 +0530

    form working fine and started with requirements generator

--- webapp/tests/tests.py
+++ webapp/tests/tests.py
@@ -1,10 +1,9 @@
 import requests
 import unittest 
   
-class endpoints(unittest.TestCase): 
-  
+class endpoints(unittest.TestCase):
     def test(self):
     	self.assertEqual(requests.get("http://0.0.0.0:8000/requirements").status_code,200)
-  
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     unittest.main()

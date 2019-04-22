4443d3d295396e538762cce05e86cb0bdf59d1dd
Aditya Thiruvengadam <T.Aditya@iiitb.org>
2019-04-23 02:31:57 +0530

    update: dockercompose generator working

--- webapp/api/utils/dockercompose_generator.py
+++ webapp/api/utils/dockercompose_generator.py
@@ -9,12 +9,29 @@ class DockerComposeGenerator:
         self.requirements_dict = requirements_dict
 
     def create_dockercompose(self):
-        pass
+        project_name = self.requirements_dict['project_name']
+        project_path = self.requirements_dict['project_path']
+        base_dockercompose_data = {
+            'version': '2.0',
+            'services': {
+                "{}".format(project_name): {
+                      "build": {"context": "."},
+                      "image": "projectmirror/{}:1.0".format(project_name),
+                      "volumes": [
+                          "{}:/{}".format(project_path, project_name),
+                      ],
+                      "restart": "on-failure"
+                },
+            }
+        }
+        path_to_dockercompose = self.generate_dockercompose(base_dockercompose_data)
+        return path_to_dockercompose
 
     def generate_dockercompose(self, data):
         path_to_dockercompose = os.path.join(self.path, 'docker-compose.yml')
         with open(path_to_dockercompose, 'w+') as f:
             yaml.dump(dict(data), f, default_flow_style=False)
+        return path_to_dockercompose
 
 
 if __name__ == '__main__':
@@ -39,4 +56,4 @@ if __name__ == '__main__':
     }
 
     dockercompose_generator = DockerComposeGenerator('./', requirements_dict)
-    dockercompose_generator.generate_dockercompose(data)
+    dockercompose_generator.create_dockercompose()

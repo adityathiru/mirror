50a5dc419a999fc6ea0a38e0619e2029613cabde
Aditya Thiruvengadam <T.Aditya@iiitb.org>
2019-04-22 19:58:55 +0530

    add: adds data path to store project data

--- webapp/api/views/index.py
+++ webapp/api/views/index.py
@@ -1,7 +1,9 @@
-from flask import jsonify, request, render_template, Response
+import os
+
 from webapp.api import app
-# from werkzeug.datastructures import ImmutableMultiDict
+from flask import jsonify, request, render_template, Response
 from webapp.api.utils.requirements_generator import RequirementsGenerator
+# from werkzeug.datastructures import ImmutableMultiDict
 
 
 def jsonify_status_code(**kw):
@@ -15,14 +17,18 @@ def requirements():
     return render_template('index.html')
 
 
-@app.route('/processing/<process_id>', methods=['POST'])
-def requirements_post(process_id):
+@app.route('/processing', methods=['POST'])
+def requirements_post():
     form_data = request.form
     dict_form_data = form_data.to_dict()
-    user_config_path = '/webapp/user/{}/'.join(process_id)
+
+    process_id = 1
+    user_config_path = '/var/lib/data/{}/'.format(process_id)
+    if not os.path.exists(user_config_path):
+        os.makedirs(user_config_path)
 
     dict_form_data["pylibs"] = request.form.getlist("pylibs")
-    dict_form_data["deep_ll"] = request.form.getlist("deep_ll")
+    dict_form_data["dl_frameworks"] = request.form.getlist("dl_frameworks")
 
     RequirementsGenerator(dict_form_data, user_config_path).python_modules_requirements()
     RequirementsGenerator(dict_form_data, user_config_path).display_requirements_dict()

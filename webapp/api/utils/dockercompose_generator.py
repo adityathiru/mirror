7539f8799f21ab3c3bae6da73dfba1ff592c76b8
bharathgane <bharath41597@gmail.com>
2019-04-23 01:10:41 +0530

    add: api/utils/dockercompose_generator.py docker compose

--- webapp/api/utils/dockercompose_generator.py
+++ webapp/api/utils/dockercompose_generator.py
@@ -2,23 +2,24 @@ import yaml
 from collections import OrderedDict 
 
 def dockercompose_generate(data):
-	with open('data.yml', 'w') as outfile:
-	    yaml.dump(dict(data), outfile, default_flow_style=False)
+    with open('data.yml', 'w') as outfile:
+        yaml.dump(dict(data), outfile, default_flow_style=False)
 
 
 if __name__ == '__main__':
-	data = {
-	      "webapp":{
-	    "build":
-	      {"context": "webapp/"},
-	    "image": "projectmirror/webapp:1.0",
-	    "volumes":[
-	      "./webapp:/webapp",
-	      "./webapp/data:/var/lib/data"],
-	    "ports":
-	      ["8000:8000"],
-	    "networks":
-	      ["elk"],
-	    "restart": "on-failure"
-	}
-	dockercompose_generate(data)
+    data = {
+          "webapp":{
+        "build":
+          {"context": "webapp/"},
+        "image": "projectmirror/webapp:1.0",
+        "volumes":[
+          "./webapp:/webapp",
+          "./webapp/data:/var/lib/data"],
+        "ports":
+          ["8000:8000"],
+        "networks":
+          ["elk"],
+        "restart": "on-failure"
+        }
+    }
+    dockercompose_generate(data)

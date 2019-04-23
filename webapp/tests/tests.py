2883d5f361b3e947442236c7e05acd99d4143ce8
bharathgane <bharath41597@gmail.com>
2019-04-24 00:10:27 +0530

    updated: tests

--- webapp/tests/tests.py
+++ webapp/tests/tests.py
@@ -1,6 +1,8 @@
 import requests
 import unittest
 from webapp.utils.tools import get_backend,validate_form
+from webapp.utils.executable_generator import ExecutableGenerator
+from webapp.utils.installation import set_default_python, install_deeplearning_frameworks,install_editors
 
 
 class endpoints(unittest.TestCase):
@@ -22,6 +24,47 @@ class endpoints(unittest.TestCase):
     	self.assertTrue(validate_form({"project_name":"Mark_123","project_path":"/user/bharath"}))
     	self.assertTrue(validate_form({"project_name":"Mark_123","project_path":"/"}))
 
+    def test_generate_function(self):
+        self.assertEqual(ExecutableGenerator("/user/bharath",{"project_name":"mark_1","project_path":"/user/bharath"}).generate_function("function_name",["function_description"]),["function_name() {\n","function_description","\n}\n"])
+        self.assertNotEqual(ExecutableGenerator("/user/bharath",{"project_name":"mark_1","project_path":"/user/bharath"}).generate_function("function_name",["function_description"]),["function_name() {\n","function_description","","\n}\n"])
+        self.assertNotEqual(ExecutableGenerator("/user/bharath",{"project_name":"mark_1","project_path":"/user/bharath"}).generate_function("function_name",["function_description"]),["function_name() {\n","function_descript","","\n}\n"])
+        self.assertNotEqual(ExecutableGenerator("/user/bharath",{"project_name":"mark_1","project_path":"/user/bharath"}).generate_function("function_name",["function_description"]),["function_name {\n","function_descript","","\n}\n"])
+        self.assertNotEqual(ExecutableGenerator("/user/bharath",{"project_name":"mark_1","project_path":"/user/bharath"}).generate_function("function_name",["function_description"]),["function_name {\n","function_descript","","\n\n"])
+
+    def test_set_default_python(self):
+        self.assertEqual(set_default_python("python2"),['RUN rm -f /usr/bin/python2 && ln -s /usr/bin/python2.7 /usr/bin/python2', 'RUN rm -f /usr/bin/python && ln -s /usr/bin/python2.7 /usr/bin/python', 'RUN wget https://bootstrap.pypa.io/get-pip.py', 'RUN python2.7 get-pip.py', 'RUN rm get-pip.py'])
+        self.assertEqual(set_default_python("python3"),['RUN rm -f /usr/bin/python3 && ln -s /usr/bin/python3.6 /usr/bin/python3', 'RUN rm -f /usr/bin/python && ln -s /usr/bin/python3.6 /usr/bin/python', 'RUN wget https://bootstrap.pypa.io/get-pip.py', 'RUN python3.6 get-pip.py', 'RUN rm get-pip.py'])
+
+    def test_install_deeplearning_frameworks(self):
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python2","cuda8"),['RUN pip install https://download.pytorch.org/whl/cu80/torch-1.0.1.post2-cp27-cp27mu-linux_x86_64.whl', 'RUN pip install torchvision', 'RUN pip install torchtext'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python2","cuda8"),['RUN pip install https://download.pytorch.org/whl/cu80/torch-1.0.1.post2-cp27-cp27mu-linux_x86_64.whl', 'RUN pip install torchvision', 'RUN pip install torchtext', 'RUN pip install tensorflow-gpu'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python3","cuda8"),['RUN pip3 install https://download.pytorch.org/whl/cu80/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python3","cuda8"),['RUN pip3 install https://download.pytorch.org/whl/cu80/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp', 'RUN pip3 install tensorflow'])
+
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python2","cuda9"),['RUN pip install torch', 'RUN pip install torchvision', 'RUN pip install torchtext'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python2","cuda9"),['RUN pip install torch', 'RUN pip install torchvision', 'RUN pip install torchtext', 'RUN pip install tensorflow-gpu'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python3","cuda9"),['RUN pip3 install torch', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python3","cuda9"),['RUN pip3 install torch', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp', 'RUN pip3 install tensorflow-gpu'])
+
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python2","cuda10"),['RUN pip install https://download.pytorch.org/whl/cu100/torch-1.0.1.post2-cp27-cp27mu-linux_x86_64.whl', 'RUN pip install torchvision', 'RUN pip install torchtext'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python2","cuda10"),['RUN pip install https://download.pytorch.org/whl/cu100/torch-1.0.1.post2-cp27-cp27mu-linux_x86_64.whl', 'RUN pip install torchvision', 'RUN pip install torchtext', 'RUN pip install tensorflow-gpu'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python3","cuda10"),['RUN pip3 install https://download.pytorch.org/whl/cu100/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python3","cuda10"),['RUN pip3 install https://download.pytorch.org/whl/cu100/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp', 'RUN pip3 install tensorflow-gpu'])
+
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python2","cpu"),['RUN pip install torch', 'RUN pip install torchvision', 'RUN pip install torchtext'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python2","cpu"),['RUN pip install torch', 'RUN pip install torchvision', 'RUN pip install torchtext', 'RUN pip install tensorflow'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch"],"python3","cpu"),['RUN pip3 install torch', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp'])
+        self.assertEqual(install_deeplearning_frameworks(["pytorch","tensorflow"],"python3","cpu"),['RUN pip3 install torch', 'RUN pip3 install torchvision', 'RUN pip3 install torchtext', 'RUN pip3 install allennlp', 'RUN pip3 install tensorflow'])
+
+    def test_install_editors(self):
+        self.assertEqual(install_editors(["vscode"],"python2"),['RUN cd / && wget https://github.com/codercom/code-server/releases/download/1.408-vsc1.32.0/code-server1.408-vsc1.32.0-linux-x64.tar.gz && tar -xzvf code-server1.408-vsc1.32.0-linux-x64.tar.gz && chmod +x code-server1.408-vsc1.32.0-linux-x64/code-server', 'COPY ./editors/docker-entrypoint.sh /usr/local/bin/', 'ENTRYPOINT ["docker-entrypoint.sh"]'])
+        self.assertEqual(install_editors(["vscode"],"python3"),['RUN cd / && wget https://github.com/codercom/code-server/releases/download/1.408-vsc1.32.0/code-server1.408-vsc1.32.0-linux-x64.tar.gz && tar -xzvf code-server1.408-vsc1.32.0-linux-x64.tar.gz && chmod +x code-server1.408-vsc1.32.0-linux-x64/code-server', 'COPY ./editors/docker-entrypoint.sh /usr/local/bin/', 'ENTRYPOINT ["docker-entrypoint.sh"]'])
+
+        self.assertEqual(install_editors(["vscode","jupyter"],"python2"),['RUN cd / && wget https://github.com/codercom/code-server/releases/download/1.408-vsc1.32.0/code-server1.408-vsc1.32.0-linux-x64.tar.gz && tar -xzvf code-server1.408-vsc1.32.0-linux-x64.tar.gz && chmod +x code-server1.408-vsc1.32.0-linux-x64/code-server', 'COPY ./editors/docker-entrypoint.sh /usr/local/bin/', 'ENTRYPOINT ["docker-entrypoint.sh"]', 'RUN pip install jupyter'])
+        self.assertEqual(install_editors(["vscode","jupyter"],"python3"),['RUN cd / && wget https://github.com/codercom/code-server/releases/download/1.408-vsc1.32.0/code-server1.408-vsc1.32.0-linux-x64.tar.gz && tar -xzvf code-server1.408-vsc1.32.0-linux-x64.tar.gz && chmod +x code-server1.408-vsc1.32.0-linux-x64/code-server', 'COPY ./editors/docker-entrypoint.sh /usr/local/bin/', 'ENTRYPOINT ["docker-entrypoint.sh"]', 'RUN pip3 install jupyter'])
+
+        self.assertEqual(install_editors(["jupyter"],"python2"),['RUN pip install jupyter'])
+        self.assertEqual(install_editors(["jupyter"],"python3"),['RUN pip3 install jupyter'])
 
 
 if __name__ == '__main__':
